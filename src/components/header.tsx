4b6811335abee616a24f30c767baf512ee3115d0
Vinicius Santos <vinicius.santos343@fatec.sp.gov.br>
2024-07-27 13:41:33 -0300

    [FIX] esLint fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,16 @@
-import NavBar from "@/components/nav/navbar";
-import ThemeSwitch from "@/components/misc/(theme)/theme-switch";
+import NavBar from '@/components/nav/navbar'
+import ThemeSwitch from '@/components/misc/(theme)/theme-switch'
 
 export default function Header() {
   return (
     <header className="flex items-center ">
       <div className="pr-2 text-2xl font-bold mr-auto">
         <span>{'{'}</span>D<span>{'}'}</span>Code
-      
       </div>
-      <NavBar/>
-      
+      <NavBar />
+
       <div className="">
-      <ThemeSwitch/>
+        <ThemeSwitch />
       </div>
     </header>
   )
